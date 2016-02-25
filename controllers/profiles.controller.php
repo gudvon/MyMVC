52edd4ee8d4d30108aec6bb9de8a4393fdbbd999
gudvon <gudvon@gmail.com>
2016-02-26 01:22:05 +0200

    profile 1.2

--- controllers/profiles.controller.php
+++ controllers/profiles.controller.php
@@ -5,24 +5,20 @@
  * Date: 13.02.2016
  * Time: 4:44
  */
-class ProfileController extends Controller {
+class ProfilesController extends Controller {
 
     public function __construct($data = array())
-    {
-        parent::__construct($data);
-        $this->model = new Message();
-    }
+{
+    parent::__construct($data);
+    $this->model = new Profile();
+}
 
-    public function index(){
+    public function user_index(){
         if ($_POST){
             if ($this->model->save($_POST)){
                 Session::setFlash('Thank you!');
             }
         }
     }
-
-    public function user_index(){
-        $this->data = $this->model->getList();
-    }
-
 }
+
--- models/profile.php
+++ models/profile.php
@@ -1,42 +1,34 @@
 <?php
 
-class Profile extends Model {
+class Profile extends Model{
 
-    public function save($data, $id = null){
-        if (!isset($data['nickname']) || isset($data['name']) || isset($data['phone'])|| isset($data['vk'])|| isset($data['facebook'])){
+    public function save($data){
+        if (!isset($data['nickname']) || !isset($data['name']) || !isset($data['phone']) || !isset($data['vk']) || !isset($data['facebook'])){
             return false;
         }
 
-        $id = (int)$id;
         $nickname = $this->db->escape($data['nickname']);
         $name = $this->db->escape($data['name']);
         $phone = $this->db->escape($data['phone']);
         $vk = $this->db->escape($data['vk']);
         $facebook = $this->db->escape($data['facebook']);
+        $login = Session::get('login');
 
-        if (!$id){
-            $sql = "
-            insert into messages
-            set name = '{$name}',
-               email = '{$email}',
-             message = '{$message}'
+        $sql = "
+            update users
+            set nickname = '{$nickname}',
+               name = '{$name}',
+               phone = '{$phone}',
+               vk = '{$vk}',
+             facebook = '{$facebook}'
+             WHERE login = '{$login}'
             ";
-        } else {
-            $sql = "
-            update messages
-            set name = '{$name}',
-               email = '{$email}',
-             message = '{$message}'
-             where id = {$id}
-            ";
-        }
 
         return $this->db->query($sql);
-
     }
 
     public function getList(){
-        $sql = "select * from messages where 1";
+        $sql = "select * from users where 1";
         return $this->db->query($sql);
     }
 
